662a7ae9f13fa58eff070235379110e76a4f8bc6
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-03-09 15:10:21 +0000

    Add accuracies (RMSE). Add quoted result print requests from question sheet. Improve variable names.
    Change lr t 0.0001, increase epochs to 400, change minibatch size to 2.
    Complete RMSE function and rounding to sig fig function.
    Fix function names. Improve variables names.

--- helper_functions.py
+++ helper_functions.py
@@ -63,7 +63,7 @@ def fit_polynomial_ls(x_t_pairs: torch.Tensor, m: int) -> torch.Tensor:
     return w_hat
 
 
-def fit_polynomial_sgd(x_t_pairs: torch.Tensor, m: int, lr=0.001, mb_size=4) -> torch.Tensor:
+def fit_polynomial_sgd(x_t_pairs: torch.Tensor, m: int, lr=0.0001, mb_size=2) -> torch.Tensor:
     """
     Implement stochastic minibatch gradient descent for fitting polynomial functions for given dataset,
     to given degree (plus bias).
@@ -96,7 +96,7 @@ def fit_polynomial_sgd(x_t_pairs: torch.Tensor, m: int, lr=0.001, mb_size=4) -> 
 
     ds = TensorDataset(x, t)
     dataloader = DataLoader(dataset=ds, batch_size=mb_size, shuffle=True)
-    num_epochs, loss = 200, 0
+    num_epochs, loss = 400, 0
     print_epochs = num_epochs / 5
     losses = list()
 
@@ -109,19 +109,17 @@ def fit_polynomial_sgd(x_t_pairs: torch.Tensor, m: int, lr=0.001, mb_size=4) -> 
             loss.backward()  # Backward pass: compute gradient of loss with respect to weights.
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=gc_max_norm)  # Gradient clipping.
             optimiser.step()  # Update weights using gradient descent.
-            # print(f'Epoch [{epoch + 1} / {num_epochs}], Batch [{batch_i + 1} / {len(dataloader)}], '
-            #       f'Loss: {loss.item()}')
         losses.append(loss.item())
         if epoch % print_epochs == 0:
-            print(f'Epoch [{epoch + 1} / {num_epochs}], Loss: {round(loss.item(), 4)}')
-    _plot_loss(losses, num_epochs, lr=lr, momentum=momentum, mb_size=mb_size, gc_max_norm=gc_max_norm)
+            print(f'Epoch [{epoch + 1} / {num_epochs}], Loss: {round_sig_figs(to_round=loss.item(), sf=4)} (to 4 s.f.)')
+    _plot_losses(losses, num_epochs, lr=lr, momentum=momentum, mb_size=mb_size, gc_max_norm=gc_max_norm)
     return model.weights.data
 
 
 # This function is identical to `fit_polynomial_sqd()`, with the addition of calculating the loss on the test set
 # at each epoch. It plots the learning curve training dataset overlapping that of the test dataset.
 def fit_polynomial_sgd_and_test(x_t_test_set_pairs: torch.Tensor, x_t_pairs: torch.Tensor, m: int,
-                                lr=0.001, mb_size=4) -> torch.Tensor:
+                                lr=0.0001, mb_size=2) -> torch.Tensor:
     """
     Implement stochastic minibatch gradient descent for fitting polynomial functions for given dataset,
     to given degree (plus bias).
@@ -161,7 +159,7 @@ def fit_polynomial_sgd_and_test(x_t_test_set_pairs: torch.Tensor, x_t_pairs: tor
 
     ds = TensorDataset(x, t)
     dataloader = DataLoader(dataset=ds, batch_size=mb_size, shuffle=True)
-    num_epochs, loss = 200, 0
+    num_epochs, loss = 400, 0
     print_epochs = num_epochs / 5
     losses, test_losses = list(), list()
 
@@ -174,44 +172,59 @@ def fit_polynomial_sgd_and_test(x_t_test_set_pairs: torch.Tensor, x_t_pairs: tor
             loss.backward()  # Backward pass: compute gradient of loss with respect to weights.
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=gc_max_norm)  # Gradient clipping.
             optimiser.step()  # Update weights using gradient descent.
-            # print(f'Epoch [{epoch + 1} / {num_epochs}], Batch [{batch_i + 1} / {len(dataloader)}], '
-            #       f'Loss: {loss.item()}')
         losses.append(loss.item())
+
+        # "report the loss periodically using printed messages"
         if epoch % print_epochs == 0:
-            print(f'Epoch [{epoch + 1} / {num_epochs}], Loss: {round_sig_figs(x=loss.item(), sf=4)}')
+            print(f'Epoch [{epoch + 1} / {num_epochs}], Loss: {round_sig_figs(to_round=loss.item(), sf=4)}')
 
         # Evaluation using test set:
         with torch.no_grad():  # Ensure no gradient computation for test data
             y_test_pred = model(x_test)  # Forward pass with test data
             test_loss = criterion(y_test_pred, t_test)  # Calculate loss on test data
         print(f'Epoch [{epoch + 1} / {num_epochs}], Loss: '
-              f'{round_sig_figs(x=loss.item(), sf=4)}, Test Loss: {round_sig_figs(x=test_loss.item(), sf=4)}')
+              f'{round_sig_figs(to_round=loss.item(), sf=4)}, Test Loss: {round_sig_figs(to_round=test_loss.item(), sf=4)}')
         test_losses.append(test_loss.item())
-    _plot_losses(test_losses, losses, num_epochs, lr=lr, momentum=momentum, mb_size=mb_size, gc_max_norm=gc_max_norm)
+    _plot_test_train_losses(test_losses, losses, num_epochs, lr=lr, momentum=momentum, mb_size=mb_size, gc_max_norm=gc_max_norm)
     return model.weights.data
 
 
-def rmse(w_or_y_true: torch.Tensor, w_or_y_pred: torch.Tensor) -> torch.Tensor:
+def rmse(w_or_y_true: torch.Tensor, w_or_y_pred: torch.Tensor) -> float:
     """
-    Calculate the root-mean-square error (RMSE) for the given weights `w` or predicted dependent variable `y`.
-    :param w_or_y_true: Ground-truth weights or predicted values.
-    :param w_or_y_pred: Computed weights or predicted values.
-    :return: Root-mean-square error (RMSE).
+    Calculate root-mean-square error (RMSE) for given weights `w` or predicted dependent variable `y`, to 4
+    significant figures.
+    :param w_or_y_true: Ground-truth weights or predicted values. Weights shapes (3,1), (4,1) or (5,1). Predicted y
+    values shapes (10, 1).
+    :param w_or_y_pred: Computed weights or predicted values. Weights shapes (3,1), (4,1) or (5,1). Predicted y values
+    shapes (10, 1).
+    :return: Root-mean-square error (RMSE), to 4 significant figures.
     """
-    return torch.sqrt(torch.mean((w_or_y_true - w_or_y_pred) ** 2))
+    size_diff = w_or_y_pred.shape[0] - w_or_y_true.shape[0]
+    if size_diff > 0:
+        zeros = torch.zeros(size_diff).unsqueeze(dim=1)
+        w_or_y_true = torch.cat((w_or_y_true, zeros), dim=0)
+    rmse = torch.sqrt(torch.mean((w_or_y_true - w_or_y_pred) ** 2))
+    rmse = round_sig_figs(to_round=float(rmse), sf=4)
+    return rmse
 
 
-def round_sig_figs(x: float, sf: int) -> float:
+def round_sig_figs(to_round: float, sf: int) -> float:
     """
     Round given number to given significant figures.
-    :param x: Float to be rounded.
+    :param to_round: Float to be rounded.
     :param sf: Number of significant figures to round to.
     :return: Given number rounded to specified number of significant figures.
     """
-    return round(x, -int(floor(log10(x))) + (sf - 1))
+    if to_round < 0:
+        rounded_x = round(to_round, -int(floor(log10(-to_round))) + (sf - 1))
+    elif to_round > 0:
+        rounded_x = round(to_round, -int(floor(log10(to_round))) + (sf - 1))
+    else:
+        rounded_x = 0
+    return rounded_x
 
 
-def _plot_loss(losses, num_epochs: int, lr: float, momentum: float, mb_size: int, gc_max_norm: float):
+def _plot_losses(losses, num_epochs: int, lr: float, momentum: float, mb_size: int, gc_max_norm: float):
     losses = np.array(losses)
     epochs = np.arange(1, num_epochs + 1)
     _, ax = plt.subplots()
@@ -228,8 +241,8 @@ def _plot_loss(losses, num_epochs: int, lr: float, momentum: float, mb_size: int
     plt.show()
 
 
-def _plot_losses(test_losses, losses, num_epochs: int, lr: float, momentum: float, mb_size: int, gc_max_norm: float):
-    losses = np.array(losses)
+def _plot_test_train_losses(test_losses, train_losses, num_epochs: int, lr: float, momentum: float, mb_size: int, gc_max_norm: float):
+    train_losses = np.array(train_losses)
     test_losses = np.array(test_losses)
     epochs = np.arange(1, num_epochs + 1)
     _, ax = plt.subplots()
@@ -237,10 +250,10 @@ def _plot_losses(test_losses, losses, num_epochs: int, lr: float, momentum: floa
     # ax.set_ylim(0, 1000000)
     plt.title(f'num_epochs={num_epochs}, lr={lr}, momentum={momentum}, mb_size={mb_size}, \n'
               f'gc_max_norm={gc_max_norm}')
-    ax.scatter(epochs, losses, label='train', color='blue', s=10)
+    ax.scatter(epochs, train_losses, label='train', color='blue', s=10)
     ax.scatter(epochs, test_losses, label='test', color='red', s=10)
 
-    ax.plot(epochs, losses, color='lightblue')
+    ax.plot(epochs, train_losses, color='lightblue')
     ax.plot(epochs, test_losses, color='salmon')
 
     plt.xlabel('epoch')
