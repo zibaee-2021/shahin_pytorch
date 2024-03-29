8408ff25dcbf18166baa9fcffc388f14fd005fc1
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-03-29 09:00:13 +0000

    task3

--- task3/task.py
+++ task3/task.py
@@ -6,15 +6,126 @@ the original network. To evaluate a modification, an ablation study can be used 
 performance before and after the modification.
 • Difference between training with the two λ sampling methods in Task 2.
 • Implement a task script “task.py”, under folder “task3”, completing the following: [30]
-o Random split the data into development set (80%) and holdout test set (20%).
-o Random split the development set into train (90%) and validation sets (10%).
-o Design at least one metric, other than the loss, on validation set, for monitoring during
+  - Random split the data into development set (80%) and holdout test set (20%).
+  - Random split the development set into train (90%) and validation sets (10%).
+  - Design at least one metric, other than the loss, on validation set, for monitoring during
 training.
-o Train two models using the two different sampling methods.
-o Report a summary of loss values, speed, metric on training and validation.
-o Save and submit these two trained models within the task folder.
-o Report a summary of loss values and the metrics on the holdout test set. Compare the results
+  - Train two models using the two different sampling methods.
+  - Report a summary of loss values, speed, metric on training and validation.
+  - Save and submit these two trained models within the task folder.
+  - Report a summary of loss values and the metrics on the holdout test set. Compare the results
 with those obtained during development.
-
-
 """
+from torch.utils.data import DataLoader, random_split
+
+import os
+from time import time
+from tqdm import tqdm
+import numpy as np
+import torch
+import torchvision.datasets as tv_datasets
+import task3_helper_functions as hfun3
+from PIL import Image
+device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+print(f'Using {device} device')
+
+if __name__ == '__main__':
+    """ ######## IMPORTANT FLAG: ACTION REQUIRED ######## """
+    # SET THIS FLAG TO TRUE IF YOU JUST WANT TO DO INFERENCE (AND NOT DO FINE-TUNING OF
+    # PRETRAINED VIT MODEL):
+    # load_finetuned_vit_for_inference_only = True
+    load_finetuned_vit_for_inference_only = False
+    """ ################################################# """
+
+    """ CHANGE SAMPLING_METHOD TO 2 FOR UNIFORM DIST IN MIXUP """
+    SAMPLING_METHOD = 1
+    # SAMPLING_METHOD = 2
+    """ ##################################################### """
+
+    if load_finetuned_vit_for_inference_only:  # DO NOT FINE-TUNE. JUST DO INFERENCE.
+        pretrained_vit, pretrained_transforms = hfun3.load_finetuned_vit_for_inference_only()
+    else:  # FINE-TUNE A PRETRAINED VIT MODEL ON CIFAR-10. FREEZE WEIGHTS AND THEN ADD LAYER TO HEAD:
+        pretrained_vit, pretrained_transforms = hfun3.load_pretrained_vit_for_finetuning()
+
+    pretrained_vit.to(device)
+
+    batch_size = 20
+    testset = tv_datasets.CIFAR10(root='./data', train=False, download=True, transform=pretrained_transforms)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    criterion = torch.nn.CrossEntropyLoss()
+    criterion = criterion.to(device)
+
+    print(f'Your flag repr(load_finetuned_vit_for_inference_only) '
+          f'is currently={load_finetuned_vit_for_inference_only}')
+
+    if load_finetuned_vit_for_inference_only:
+        # INFERENCE (ON TEST-SET) ONLY:
+        print(f'One-off inference only')
+        test_loss, test_acc, test_mins = hfun3.test_inference(device, pretrained_vit, testloader, criterion)
+    else:
+        # FINE-TUNE & EVALUATE on 80%, EVALUATE ON 20% HOLDOUT TEST SET FOR 20 EPOCHS:
+        print('20 epochs of fine-tuning and evalutation of prediction accuracy on test set, per epoch.')
+        trainset = tv_datasets.CIFAR10(root='./data', train=True, download=True, transform=pretrained_transforms)
+        full_dataset = torch.utils.data.ConcatDataset([trainset, testset])
+        train_size = int(0.8 * len(full_dataset))
+        test_size = len(full_dataset) - train_size
+        trainset, testset = random_split(full_dataset, [train_size, test_size])
+        validation_size = int(0.1 * len(trainset))
+        train_size = len(trainset) - validation_size
+        trainset, validationset = random_split(trainset, [train_size, validation_size])
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
+        validationloader = torch.utils.data.DataLoader(validationset, batch_size=batch_size, shuffle=False, num_workers=2)
+        testloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False, num_workers=2)
+
+        opt = torch.optim.Adam(pretrained_vit.parameters(), lr=0.003)
+        epochs = 20
+        train_losses = torch.zeros(epochs)
+        val_losses = torch.zeros(epochs)
+        test_losses = torch.zeros(epochs)
+        train_accs = torch.zeros(epochs)
+        val_accs = torch.zeros(epochs)
+        test_accs = torch.zeros(epochs)
+        train_mins = torch.zeros(epochs)
+        val_mins = torch.zeros(epochs)
+        test_mins = torch.zeros(epochs)
+        all_20_epochs_start = time()
+        for epoch in tqdm(range(epochs)):
+            print(f'\nEpoch#{epoch + 1}')
+            # 1. FINE-TUNE PRETRAINED MODEL:
+            train_loss_per_epoch, train_accuracy, train_min = hfun3.fine_tune(device, pretrained_vit, trainloader,
+                                                                              criterion, opt, epoch, SAMPLING_METHOD)
+            train_losses[epoch] = train_loss_per_epoch
+            train_accs[epoch] = train_accuracy
+            train_mins[epoch] = train_min
+            # 2. EVALUATE ON VALIDATION SET AFTER EACH EPOCH OF FINE-TUNING:
+            val_loss_per_epoch, val_accuracy, val_min = hfun3.test_inference(device, pretrained_vit,
+                                                                             validationloader, criterion)
+            val_losses[epoch] = val_loss_per_epoch
+            val_accs[epoch] = val_accuracy
+            val_mins[epoch] = val_min
+            # 3. EVALUATING ON TEST AT EVERY EPOCH IS NOT ASKED FOR (BUT I DON'T SEE HARM IN DOING THIS), IT'S VERY QUICK:
+            # TEST-SET AFTER EACH EPOCH OF FINE-TUNING:
+            test_loss_per_epoch, test_accuracy, test_min = hfun3.test_inference(device, pretrained_vit,
+                                                                                 testloader, criterion)
+            test_losses[epoch] = test_loss_per_epoch
+            test_accs[epoch] = test_accuracy
+            test_mins[epoch] = test_min
+        # # SAVE LOSSES & ACCURACIES FOR EACH OF 20 EPOCHS TO CSV FILES:
+        hfun3.save_loss_acc_mins_to_csv(SAMPLING_METHOD, train_losses, val_losses, test_losses,
+                                        train_accs, val_accs, test_accs, train_mins, val_mins, test_mins)
+
+        print(f'Classification accuracy per epoch for test set= {test_losses}')
+        print(f'END - Fine-tuning model for {epochs} epochs took {round(((time() - all_20_epochs_start) / 60), 4)} mins')
+
+        """ ######## IMPORTANT DON'T CHANGE THIS FLAG ######## 
+        UNLESS YOU'RE 100% SURE ABOUT OVERWRITING MY FINE-TUNED MODELS 
+        OTHERWISE YOU SHOULD MANUALLY CHANGE THE FILE NAMES"""
+        save_fine_tuned_model = False
+        if save_fine_tuned_model:
+            tuned_model_dirs = f'saved_models/pretrained_finetuned/sm_{SAMPLING_METHOD}'
+            if not os.path.exists(tuned_model_dirs): os.makedirs(tuned_model_dirs)
+            fine_tuned_path = os.path.join(tuned_model_dirs, 'vit_finetuned.pt')
+            torch.save(pretrained_vit.state_dict(), fine_tuned_path)
+            print('Trained model saved.')
+
+
---
+++ task3/task3_helper_functions.py
@@ -0,0 +1,195 @@
+import os
+from time import time
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as tv_transforms
+import torchvision.datasets as tv_datasets
+from PIL import Image
+
+
+class MixUp(nn.Module):
+
+    def augment(self, device, X, y, batch_size, sampling_method, alpha=0.2):
+        """
+        If sampling_method is 1: λ is sampled from a beta distribution as described in Zhang et al 2018.
+        If sampling_method is 2: λ is sampled uniformly from a predefined range.
+
+        "For mixup, we find that αlpha ∈ [0.1, 0.4] leads to improved performance over ERM,
+        whereas for large αlpha, mixup leads to underfitting." Zhang et al.
+        """
+        np.random.seed(42)
+
+        if sampling_method == 2:
+            lambda_ = np.random.uniform(low=0.0, high=1.0)
+        else:
+            lambda_ = np.random.beta(alpha, alpha)
+
+        lam = torch.tensor(lambda_, device=device)
+
+        random_i = torch.randperm(batch_size).to(device)
+        X2 = X[random_i, :, :, :]
+        y2 = y[random_i]
+
+        y = F.one_hot(y, num_classes=10) * 1.0
+        y2 = F.one_hot(y2, num_classes=10) * 1.0
+        new_X = (lam * X) + ((1. - lam) * X2)
+        new_y = (lam * y) + ((1. - lam) * y2)
+
+        return new_X, new_y
+
+
+def load_finetuned_vit_for_inference_only():
+    print('You are loading an already fine-tuned (CIFAR-10) pretrained ViT model for inference only.')
+    pretrained_vit = torchvision.models.vit_b_16()
+    pretrained_vit.heads = nn.Sequential(nn.Linear(in_features=768, out_features=10))
+    # print('\nWeights before loading saved model:')
+    # print(pretrained_vit.heads[0].weight.data)
+    saved_model_path = 'saved_models/pretrained_finetuned/vit_finetuned.pt'
+    pretrained_vit.load_state_dict(torch.load(saved_model_path, map_location=torch.device('cuda')))
+    # print('\nWeights after loading saved model:')
+    # print(pretrained_vit.heads[0].weight.data)
+    pretrained_transforms = tv_transforms.Compose([
+        tv_transforms.Resize((224, 224)),
+        tv_transforms.ToTensor(),
+        tv_transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+    return pretrained_vit, pretrained_transforms
+
+
+def load_pretrained_vit_for_finetuning():
+    print("You're loading a pretrained ViT, in order to fine-tune it on CIFAR-10 over 20 epochs "
+          "of a training loop. Evaluate the model on a test set at each epoch.")
+    pretrained_vit_weights = torchvision.models.ViT_B_16_Weights.DEFAULT
+    pretrained_vit = torchvision.models.vit_b_16(weights=pretrained_vit_weights)
+    pretrained_transforms = pretrained_vit_weights.transforms()
+    # FREEZE MODEL PARAMETERS TO PERFORM TRANSFER LEARNING (I.E FINE-TUNING):
+    for params in pretrained_vit.parameters():
+        params.requires_grad = False
+    pretrained_vit.heads = nn.Sequential(nn.Linear(in_features=768, out_features=10))
+    return pretrained_vit, pretrained_transforms
+
+
+def test_inference(device, pretrained_vit, testloader, criterion, epoch=None):
+    test_start = time()
+    pretrained_vit.eval()
+    test_loss_per_epoch, test_accuracy = 0, 0
+
+    with torch.inference_mode():
+        for i, data in enumerate(testloader):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            y_preds = pretrained_vit(inputs)
+            # y_preds.shape is .  these are   logits.
+            loss = criterion(y_preds, labels)
+
+            test_pred_labels = y_preds.argmax(dim=1)
+            test_accuracy += ((test_pred_labels == labels).sum().item()/len(test_pred_labels))
+            test_loss_per_epoch += loss.item()
+            if epoch and i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, test_loss_per_epoch / 2000))
+                test_loss_per_epoch = 0.0
+    test_loss_per_epoch = test_loss_per_epoch / len(testloader)
+    print(f'test_loss_per_epoch={test_loss_per_epoch}')
+    test_accuracy = test_accuracy / len(testloader)
+    print(f'test_accuracy={test_accuracy}')
+    test_min = round((time() - test_start) / 60, 4)
+    print(f'Test/inference took {test_min} mins')
+    return test_loss_per_epoch, test_accuracy, test_min
+
+
+def fine_tune(device, pretrained_vit, trainloader, criterion, opt, epoch,
+              sampling_method):
+    train_start = time()
+    pretrained_vit.train()
+    train_loss_per_epoch, train_accuracy = 0, 0
+    mix_up = MixUp()
+    for i, data in enumerate(trainloader):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        X, y = mix_up.augment(device=device, X=inputs, y=labels, sampling_method=sampling_method,
+                              batch_size=inputs.shape[0])
+        opt.zero_grad()
+        y_preds = pretrained_vit(X)
+        y_preds_class = torch.argmax(torch.softmax(y_preds, dim=1), dim=1)
+        y = torch.argmax(y, dim=1)  # convert one-hot back to original
+        train_accuracy += (y_preds_class == y).sum().item() / len(y_preds)
+        loss = criterion(y_preds, y)
+        loss.backward()
+        opt.step()
+
+        train_loss_per_epoch += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, train_loss_per_epoch / 2000))
+
+    train_loss_per_epoch = train_loss_per_epoch / len(trainloader)
+    print(f'train_loss_per_epoch={train_loss_per_epoch}')
+    train_accuracy = train_accuracy / len(trainloader)
+    print(f'train_accuracy={train_accuracy}')
+    train_min = round((time() - train_start) / 60, 4)
+    print(f'One epoch of tuning took {train_min} mins')
+    return train_loss_per_epoch, train_accuracy, train_min
+
+
+def validation(device, pretrained_vit, validationloader, criterion, epoch=None):
+    val_start = time()
+    pretrained_vit.eval()
+    val_loss_per_epoch, val_accuracy = 0, 0
+
+    with torch.inference_mode():
+        for i, data in enumerate(validationloader):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            y_preds = pretrained_vit(inputs)
+            loss = criterion(y_preds, labels)
+            val_pred_labels = y_preds.argmax(dim=1)
+            val_accuracy += ((val_pred_labels == labels).sum().item()/len(val_pred_labels))
+            val_loss_per_epoch += loss.item()
+            if epoch and i % 2000 == 1999:
+                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, val_loss_per_epoch / 2000))
+                val_loss_per_epoch = 0.0
+    val_loss_per_epoch = val_loss_per_epoch / len(validationloader)
+    print(f'val_loss_per_epoch={val_loss_per_epoch}')
+    val_accuracy = val_accuracy / len(validationloader)
+    print(f'val_accuracy={val_accuracy}')
+    val_min = round((time() - val_start) / 60, 4)
+    print(f'Validation took {val_min} mins')
+    return val_loss_per_epoch, val_accuracy, val_min
+
+
+def save_loss_acc_mins_to_csv(sampling_method, train_losses, val_losses, test_losses,
+                              train_accs, val_accs, test_accs, train_mins, val_mins, test_mins):
+    train_losses_np = train_losses.cpu().numpy()
+    val_losses_np = val_losses.cpu().numpy()
+    test_losses_np = test_losses.cpu().numpy()
+    train_accs_np = train_accs.cpu().numpy()
+    val_accs_np = val_accs.cpu().numpy()
+    test_accs_np = test_accs.cpu().numpy()
+    train_mins_np = train_mins.cpu().numpy()
+    val_mins_np = val_mins.cpu().numpy()
+    test_mins_np = test_mins.cpu().numpy()
+    losses_accs_mins_dirs = f'saved_models/acc_losses_mins/sm_{sampling_method}'
+    if not os.path.exists(losses_accs_mins_dirs): os.makedirs(losses_accs_mins_dirs)
+    vit_train_losses_path = os.path.join(losses_accs_mins_dirs, 'train_losses_np.csv')
+    vit_val_losses_path = os.path.join(losses_accs_mins_dirs, 'val_losses_np.csv')
+    vit_test_losses_path = os.path.join(losses_accs_mins_dirs, 'test_losses_np.csv')
+    vit_train_accs_path = os.path.join(losses_accs_mins_dirs, 'train_accs_np.csv')
+    vit_val_accs_path = os.path.join(losses_accs_mins_dirs, 'val_accs_np.csv')
+    vit_test_accs_path = os.path.join(losses_accs_mins_dirs, 'test_accs_np.csv')
+    vit_train_mins_path = os.path.join(losses_accs_mins_dirs, 'train_mins_np.csv')
+    vit_val_mins_path = os.path.join(losses_accs_mins_dirs, 'val_mins_np.csv')
+    vit_test_mins_path = os.path.join(losses_accs_mins_dirs, 'test_mins_np.csv')
+    np.savetxt(vit_train_losses_path, train_losses_np, delimiter=',')
+    np.savetxt(vit_val_losses_path, val_losses_np, delimiter=',')
+    np.savetxt(vit_test_losses_path, test_losses_np, delimiter=',')
+    np.savetxt(vit_train_accs_path, train_accs_np, delimiter=',')
+    np.savetxt(vit_val_accs_path, val_accs_np, delimiter=',')
+    np.savetxt(vit_test_accs_path, test_accs_np, delimiter=',')
+    np.savetxt(vit_train_mins_path, train_mins_np, delimiter=',')
+    np.savetxt(vit_val_mins_path, val_mins_np, delimiter=',')
+    np.savetxt(vit_test_mins_path, test_mins_np, delimiter=',')
+    print(f'saved to {losses_accs_mins_dirs}')
+
+
+if __name__ == '__main__':
+    print('start')
+
